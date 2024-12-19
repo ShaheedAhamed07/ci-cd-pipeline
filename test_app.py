86a01b474ac01306ba5a0568a413d40675f977ca
Shaheed Ahmed <32387@yenepoya.edu.in>
2024-12-19 21:19:14 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -10,6 +10,7 @@ self.client = app.test_client()
 def test_hello(self):
 response = self.client.get("/") self.assertEqual(response.status_code, 200) self.assertEqual(response.data.decode(), "Hello, World!")
 
-if  name == " main ": unittest.main()
+if  name == " main ":
+  unittest.main()
 
 
