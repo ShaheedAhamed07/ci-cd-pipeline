e84c1a6346aa17e0b855b38f83be842ff08b20ee
Shaheed Ahmed <32387@yenepoya.edu.in>
2024-12-19 21:26:50 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,10 +3,10 @@ import unittest
 from app import app
 
 class TestApp(unittest.TestCase):
-def setUp(self):
+  def setUp(self):
     self.client = app.test_client()
 
-def test_hello(self):
+  def test_hello(self):
     response = self.client.get("/")
     self.assertEqual(response.status_code, 200)
     self.assertEqual(response.data.decode(), "Hello, World!")
