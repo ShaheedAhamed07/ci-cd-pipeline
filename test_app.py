58dae29f12dd948469e38b111774a7463400d931
Shaheed Ahmed <32387@yenepoya.edu.in>
2024-12-19 21:22:21 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -8,7 +8,9 @@ def setUp(self):
 
 
 def test_hello(self):
-response = self.client.get("/") self.assertEqual(response.status_code, 200) self.assertEqual(response.data.decode(), "Hello, World!")
+    response = self.client.get("/")
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.data.decode(), "Hello, World!")
 
 if  name == " main ":
   unittest.main()
