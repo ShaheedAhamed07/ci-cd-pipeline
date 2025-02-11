459176cd3b56cc574191b87f18f91cc52aca8fb1
Shaheed Ahmed <32387@yenepoya.edu.in>
2025-02-11 10:48:21 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -8,7 +8,7 @@ class TestApp(unittest.TestCase):
     def test_hello(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode(), "Hello, World!")
+        self.assertEqual(response.data.decode(), "Hello")
 
 if __name__ == "__main__":
     unittest.main()
